0451cc6d1360e559a871cd6f485a929a5c55e542
shanemol-lab <mixes_fad_0v@icloud.com>
2026-02-15 13:08:32 +1300

    removed cardscoreapp.swift as no longer used

--- UpDownRiverScorer/UpDownRiverScorer/CardScoreApp.swift
+++ UpDownRiverScorer/UpDownRiverScorer/CardScoreApp.swift
@@ -1,10 +1,2 @@
-// CardScoreApp has been disabled as an app entry point. Use UpDownRiverScorerApp as the single @main entry.
-// This file remains for reference and can be removed from the build target.
 
-import SwiftUI
 
-struct CardScoreApp_Previews: PreviewProvider {
-    static var previews: some View {
-        GameListView()
-    }
-}
